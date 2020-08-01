c3d963c442f1edad2492849b447d6b0a5773d861
hx08371 <brammannjeffreyt@johndeere.com>
2020-08-01 18:36:21 -0500

    My Program WORKS

--- controllers/triviaController.js
+++ controllers/triviaController.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models").User;
 const Category = require("../models").Category;
-const TriviaModel = require("../models").Trivia;
+const TriviaModel = require("../models").TriviaQuestions;
 
 // New route - send empty form
 router.get("/new", (req, res) => {
