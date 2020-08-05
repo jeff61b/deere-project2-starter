df752d263d9970006aaddcc05fb2108cc9863988
hx08371 <brammannjeffreyt@johndeere.com>
2020-08-04 19:24:18 -0500

    completed my MVP. working on CSS

--- controllers/triviaController.js
+++ controllers/triviaController.js
@@ -31,46 +31,72 @@ router.post("/", (req, res) => {
   });
 });
 
-// Add New Trivia Question
-// router.post("/", (req, res) => {
-//   TriviaModel.create(req.body).then((newQuestion) => {
-//     console.log("new" + newQuestion);
-//     res.redirect("/");
-//   });
-// });
-
 // SHOW ROUTE - GET ONE Trivia Question
-router.get("/:id", (req, res) => {
-  TriviaModel.findByPk(req.params.id, {
-    include: [
-      {
-        model: User,
-        attributes: ["name"],
-      },
-      {
-        model: Category,
-      },
-    ],
-    attributes: [
-      "question",
-      "answer1",
-      "answer2",
-      "answer3",
-      "answer4",
-      "correctAnswer",
-      "categoryId",
-    ],
-  }).then((trivia) => {
-    res.render("show.ejs", {
-      trivia: trivia,
+router.get("/:id", function (req, res) {
+  TriviaModel.findByPk(req.params.id).then((foundQuestion) => {
+    Category.findByPk(foundQuestion.categoryId).then((allCategories) => {
+      //console.log(allCategories);
+      console.log("New show route");
+      //console.log(foundQuestion);
+      console.log(foundQuestion.categoryId);
+      console.log(allCategories.name);
+      res.render("show.ejs", {
+        trivia: foundQuestion,
+        categories: allCategories,
+      });
     });
   });
 });
+
+// // SHOW ROUTE - GET ONE Trivia Question
+// router.get("/:id", function (req, res) {
+//   TriviaModel.findByPk(req.params.id).then((foundQuestion) => {
+//     Category.findAll().then((allCategories) => {
+//       //console.log(allCategories);
+//       console.log("New show route");
+//       console.log(foundQuestion);
+//       //console.log(trivia.categoryId);
+//       res.render("show.ejs", {
+//         trivia: foundQuestion,
+//         categories: allCategories,
+//       });
+//     });
+//   });
+// });
+
+// router.get("/:id", (req, res) => {
+//   console.log("show route");
+//   TriviaModel.findByPk(req.params.id, {
+//     include: [
+//       {
+//         model: Category,
+//       },
+//     ],
+//     attributes: [
+//       "id",
+//       "question",
+//       "answer1",
+//       "answer2",
+//       "answer3",
+//       "answer4",
+//       "correctAnswer",
+//       "categoryId",
+//     ],
+//   }).then((trivia) => {
+//     console.log("render show form");
+//     console.log(trivia.name);
+
+//     res.render("show.ejs", {
+//       trivia: trivia,
+//     });
+//   });
+// });
 
 // Edit
 router.get("/:id/edit", function (req, res) {
   TriviaModel.findByPk(req.params.id).then((foundQuestion) => {
     Category.findAll().then((allCategories) => {
+      console.log(allCategories);
       res.render("edit.ejs", {
         trivia: foundQuestion,
         categories: allCategories,
